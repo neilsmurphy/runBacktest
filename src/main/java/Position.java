fe3b7278c55812c2f8d7c857406b1f6207e4ffb1
Neil Murphy <neil@neilmurphy.tech>
2021-07-11 22:47:03 -0400

    Position updated.

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,29 +1,30 @@
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * The Position class tracks all positions. It keeps a list of all the
  * position objects created. It can provide open or closed or all positions.
  * It will provide  position event when positions are created or closed.
- * A position is closed when it equals zero. For instance, if you go from
+ * A position is closed when it equals zero.
  */
 public class Position {
     private static final AtomicInteger count = new AtomicInteger(0);
     public int positionId;
+    public int barCreated;
     double quantity = 0;
     public TradeData tradeData;
     Deque<Transaction> transactions = new LinkedList<>();
 
-    Position (int i, TradeData td) {
+    Position (int index, TradeData tradeData) {
         positionId = count.incrementAndGet();
-        tradeData = td;
+        barCreated = index;
+        this.tradeData = tradeData;
     }
 
-    public void addTransaction (int i, TradeData td, double prc, double qty) {
-        transactions.add(new Transaction(i, td, prc, qty));
-        quantity += qty;
+    public void addTransaction (int index, TradeData tradeData, double price, double quantity) {
+        transactions.add(new Transaction(index, tradeData, price, quantity));
+        this.quantity += quantity;
     }
 
     public TradeData getTradeData () {
@@ -33,6 +34,5 @@ public class Position {
     public double getQuantity() {
         return quantity;
     }
-
 }
 
